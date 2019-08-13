ad0266400a9322bb4cdc9c7c0a9606790c8d9fda
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2019-08-13 12:54:30 +0200

    Changed the prediction Method to just a distribution estimation

--- src/detect_anomalies.py
+++ src/detect_anomalies.py
@@ -2,15 +2,15 @@
 from data_caster import transpose
 from classifier import Classifier as CL
 from database_adapter import DBAdapter as DB
-from isolation_forest import IsolationForest as ML
+
+# from isolation_forest import IsolationForest as ML
+from distribution_estimator import DistributionEstimator as ML
 
 
 def detect_anomalies(input_db_settings, read_settings, training_settings, analyisis_settings, model_settings, classification_settings, output_db_settings, write_settings):
 
     db_in = DB(input_db_settings)
     data = db_in.get_data(read_settings)
-    
-    print(data)
 
     ml = ML(model_settings, read_settings)
 
@@ -20,7 +20,7 @@ def detect_anomalies(input_db_settings, read_settings, training_settings, analyi
 
     scores  = ml.analyze(data, analyisis_settings)
 
-    result = CL(classification_settings).classify(scores)
+    result = ml.classify(scores, classification_settings)
 
     db_out = DB(output_db_settings)
     db_out.write(result, write_settings, read_settings)
---
+++ src/distribution_estimator.py
@@ -0,0 +1,74 @@
+
+import sys
+import random
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+
+from logger import logger
+from ml_template import ML_template
+
+from pprint import pprint
+
+class DistributionEstimator(ML_template):
+
+    def __init__(self, model_settings, read_settings):
+        logger.info("Using model [DistributionEstimator]")
+        self.path  = model_settings.pop("model_file_format").format(**read_settings)
+        logger.info("The model path is [{path}]".format(path=self.path))
+        self.model_settings = model_settings
+        self.models = {}
+
+    def needs_training(self):
+        return True
+
+    def _stack_data(self, values):
+        return np.column_stack([v for k, v in values.items() if k != "time"])
+
+    def train(self, data, settings):
+        self._check_data(data, settings["min_n_of_data_points"])
+        logger.info("Training the Models")
+
+        for selector, values in data.items():
+            self.models[selector] = {
+                "loc":np.mean(self._stack_data(values), axis=0),
+                "scale":np.std(self._stack_data(values), axis=0)
+            }
+
+        logger.info("Models Trained")
+
+    
+    def set_seed(self, seed):
+        logger.info("Setting the seed to [{seed}]".format(**locals()))
+        random.seed(seed)
+        np.random.seed(seed)
+
+    def analyze(self, data, settings):
+        self._check_data(data, settings["min_n_of_data_points"])
+        logger.info("Setting the seed for reproducibility porpouses.")
+        self.set_seed(settings["seed"])
+        logger.info("Predicting the average anomalies scores for the data")
+        output = data.copy()
+
+        for selector, values in data.items():
+            # Calculate the - log probability of the values assuming that they are independent (Usually just 1 feature so reasonable)
+            output[selector]["score"] = -np.sum(norm.logpdf(self._stack_data(values), **self.models[selector]), axis=1)
+
+        return output
+
+    
+    def classify(self, data, classification_settings):
+        logger.info("Classifying the scores")
+        for selector, points in data.items():
+            values = points["score"]
+            # Calc the two thresholds for normals and anomalies
+            normals   = np.nanquantile(values, classification_settings["normal_percentage"])
+            logger.info("The normal quantile for the selector [{selector}] is [{normals}]".format(**locals()))
+            anomalies = np.nanquantile(values, classification_settings["anomaly_percentage"])
+            logger.info("The anomaly quantile for the selector [{selector}] is [{anomalies}]".format(**locals()))
+            # By default values are possible anomalies
+            result = np.ones_like(values)
+            result[values < normals] = 0
+            result[values > anomalies] = 2
+            data[selector]["class"] = result
+        return data
