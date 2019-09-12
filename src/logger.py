48827cf0f860987f49cf37d199bac0d896e97bbe
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2019-09-12 16:48:59 +0200

    Changed pre-argument-parsing default logging level to ERROR

--- src/logger.py
+++ src/logger.py
@@ -5,10 +5,10 @@ import logging
 
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(levelname)s:%(filename)s:%(funcName)s:%(lineno)d:%(message)s') # %(asctime)s:
 handler.setFormatter(formatter)
 logger.addHandler(handler)
